55daecaeee765d1fe55fa8b9703096488f8c5101
Marcus Wyatt <mlwyatt2008@gmail.com>
2016-04-20 21:08:43 -0400

    remove unneeded stuff

--- db/migrate/20160305033220_create_boards.rb
+++
@@ -1,9 +0,0 @@
-class CreateBoards < ActiveRecord::Migration
-  def change
-    create_table :boards do |t|
-      t.string :hint
-      t.string :answer
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20160305035425_delete_board_table.rb
+++
@@ -1,5 +0,0 @@
-class DeleteBoardTable < ActiveRecord::Migration
-  def change
-    drop_table :boards
-  end
-end
